899574ca723ab1a3b7172e9ab092bdff5ed54cf6
Yancey Leo <yanceyofficial@gmail.com>
2019-12-08 23:15:11 +0800

    feat: learn devServer

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -34,7 +34,7 @@ const configFactory = (env: string): Configuration => {
 
     resolve: {
       // 在导入模块时, 如果模块是下列扩展名, 将不用写文件后缀
-      extensions: ['.wasm', '.mjs', '.json', '.js', '.jsx', '.ts', '.tsx'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
 
       // 定义别名, 配合 tsconfig.json 一起食用
       alias: {
@@ -103,7 +103,6 @@ const configFactory = (env: string): Configuration => {
       // webpack v4+ 提供的全新的通用分块策略
       splitChunks: {
         chunks: 'all',
-        name: false,
       },
 
       // 为每个仅含有 runtime 的入口起点添加一个额外 chunk
@@ -132,9 +131,104 @@ const configFactory = (env: string): Configuration => {
 
     devtool: !isEnvProduction ? 'cheap-module-eval-source-map' : undefined,
 
-    devServer: isEnvProduction
+    devServer: isEnvDevelopment
       ? {
+          // 端口号
           port: 3000,
+
+          // 开发环境的 host
+          host: 'localhost',
+
+          // 是否启用压缩
+          compress: true,
+
+          // 用于解决 SPA 刷新 404 的问题
+          historyApiFallback: {
+            disableDotRule: true,
+          },
+
+          // 指定静态文件的路径
+          contentBase: path.join(__dirname, 'public'),
+
+          // 配置 CORS
+          allowedHosts: ['.yanceyleo.com', '.yancey.app'],
+
+          // 启动服务后打开浏览器, 貌似不好使
+          open: true,
+
+          // 在有警告或者编译错误时, 错误信息全屏遮盖浏览器
+          overlay: false,
+
+          // 启用 webpack 的模块热替换功能
+          hot: true,
+
+          // 热更换的信息不会打印到控制台
+          quiet: true,
+
+          // 允许浏览器使用本地 IP 打开
+          useLocalIp: true,
+
+          // 只在控制台打印如下信息之一, 但在 quiet 模式无效
+          // 'none' | 'errors-only' | 'minimal' | 'normal' | 'verbose' object
+          // stats: 'errors-only',
+
+          // 在构建失败时, 不需要将页面刷新作为回退
+          // hotOnly: true,
+
+          // lazy 模式, 不会监听任何文件的改动, 除非主动拉取
+          // lazy: true,
+
+          // 配合 lazy 模式使用, 可以只在某个文件被请求时编译
+          // filename: 'bundle.js',
+
+          // 代理请求, 下面例子中, 请求到的 /api/xxx 会被代理到请求 http://localhost:3000/api/xxx 上
+          // proxy: {
+          //   '/api': {
+          //     target: 'http://localhost:3000',
+
+          //     pathRewrite: { '^/api': '' },
+
+          //     // 默认不接受不安全的后端请求, 关闭 secure 可解决
+          //     secure: false,
+
+          //     // 下面的例子中, /api 代理到本地服务器, 但index.html 还是接收服务器的, 可以使用 bypass
+          //     bypass: function(req, res, proxyOptions) {
+          //       if (req.headers.accept.indexOf('html') !== -1) {
+          //         console.log('Skipping proxy for browser request.')
+          //         return '/index.html'
+          //       }
+          //     },
+          //   },
+          // },
+
+          // 你也可以代理多个路径
+          // proxy: [
+          //   {
+          //     context: ['/auth', '/api'],
+          //     target: 'http://localhost:3000',
+          //   },
+          // ],
+
+          // 请求前的钩子
+          // before: function(app, server) {
+          //   app.get('/some/path', function(req, res) {
+          //     res.json({ custom: 'response' })
+          //   })
+          // },
+
+          // 请求后的钩子
+          // after: function(app, server) {
+          // },
+
+          // 添加响应头
+          // headers: [],
+
+          // 开发服务器支持 https
+          // https:[
+          //   key: fs.readFileSync('/path/to/server.key'),
+          //   cert: fs.readFileSync('/path/to/server.crt'),
+          //   ca: fs.readFileSync('/path/to/ca.pem'),
+          // ]
         }
       : undefined,
   }
