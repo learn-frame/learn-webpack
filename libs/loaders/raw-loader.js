e8d9eb54dc88f350d48cb3a42860d9357fb55943
YanceyOfficial <yanceyofficial@gmail.com>
2020-01-20 19:08:42 +0800

    feat: learn how to write a loader

--- libs/loaders/raw-loader.js
+++ libs/loaders/raw-loader.js
@@ -1,3 +1,9 @@
-const { runLoaders } = require('loader-runner')
 
-module.exports = {}
+
+module.exports = function(source) {
+  const result = JSON.parse(source)
+    .replace(/\u2028/g, '\\u2028')
+    .replace(/\u2029/g, '\\u2029')
+
+  return `export default ${result}`
+}
